37a1b04cc7b85299608e3b7d0a751e5e124bc935
Willams <84189241+willams502@users.noreply.github.com>
2021-09-30 11:01:45 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+
+
+
+var d = new Date();
+alert(d.getDate());
+
+
+//var count;
+//for(count 0; count <= 5; count++){
+//	alert(count);
+//};
+
+
+//var count = 0;
+//while (count <5){
+//	console.log(count);
+//	count ++;
+//	alert(count);
+//};
+
+
+//var idade = prompt("Qual a sua idade"); 
+//if(idade >= 18){
+//	alert("Maior de idade")
+//} else{
+//	alert("menor de idade")
+//}
+
+
+//var fruta = [{nome:"Banana", cor:"amarela"}, {nome:"uva", cor"roxa"}];
+
+//console.log(fruta.nome);
+
+//console.log(fruta.cor);
+
+//alert(fruta[1].cor);
+	
+//var lista = ["banana", "üva", 'laranjja'];
+
+//lista.push('uva');
+
+//console.log(lista);
+
+//console.log(lista.toString());
+
+//console.log(lista.join(" - "));
+
+//console.log(lista.length);
+
+//console.log(lista.reverse());
+
+//var nome = "Matheus Willams";
+
+//var idade = 23;
+
+//var idade2 =10;
+
+//var frase = "Japao e o melhor time do mundo"
+
+//alert('Bem vindo ' + nome + " Voce tem  "+ idade);
+
+//alert(idade2 + idade);
+
+//console.log("Matheus");
+
+//console.log(frase.replace("Japao", "brasil"));console.log(idade - idade2);
